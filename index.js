2e01714bee7fb8f2f5b5f94d5085880c34e1e5bc
Jorge Ariel Castillo <j.a.castillo.jc@gmail.com>
2022-10-15 00:57:23 -0300

    intente renderizar y filtrar las categorias pero me da bug

--- index.js
+++ index.js
@@ -1,15 +1,75 @@
-const request = async () => {
-    try {
-        const response = await fetch("./assets/menu.json");
-        const data = await response.json();
-        console.log(data)
-        console.log(data[5].Categoria, data[5].id, data[5].Nombre,data[5].Descripcion, data[5].Precio )
-        return data;  
+const productContainer = document.getElementById('products');
 
-    } catch {
-        console.log("no se recibe nada"); 
-    }
+const titulo = document.querySelector('.populares');
+
+let categoryBtn = document.querySelector('.categoriesCard')
+
+const categoriesList = document.querySelectorAll('.category')
+// let cart = JSON.parse(localStorage.getItem('cart') || []);
+
+const renderProducts = (product) => {
+    const {id, Nombre, Descripcion, Precio, img, Categoria} = product;
+    titulo.textContent = `${Categoria.toUpperCase()}`
+    productContainer.innerHTML += `
+    <div class="productCard">
+    <img src="${img}" alt="" />
+    <div class="productCardDescription">
+      <h3>${Nombre}</h3>
+      <p>${Descripcion}</p>
+    </div>
+    <div class="productCardPrice">
+      <p>$${Precio}</p>
+      <button id="productCardBtn"
+      data-id="${id}"
+      data-nombre="${Nombre}"
+      data-precio="${Precio}"
+      data-img="${img}">Agregar</button>
+    </div>
+    `
+}
+
+const categoryState = (selectedCategory) =>{
+    const categories = [...categoriesList];
+    categories.forEach((categoryBtn) => {
+        if(categoryBtn.dataset.category !== selectedCategory){
+            categoryBtn.classList.remove('active');
+            return
+        }
+        categoryBtn.classList.add('active');
+    })
 
 }
 
-request()
+const changeFilter = (e) => {
+    const selectedCategory = e.target.dataset.category;
+    console.log(e.dataset.category)
+    categoryState(selectedCategory);
+}
+
+const renderFilter = async (category) => {
+    const menu = await request();
+    const productsList = menu.filter(
+        (product) => product.Categoria.toUpperCase() === category.toUpperCase());
+    let producto = productsList.map(renderProducts).join('');
+
+}
+
+const applyFilter = (e) => {
+    renderFilter(e.target.dataset.category)
+    if (!e.target.dataset.classList.contains('category')) return;
+    categoryState(e.target.dataset.category);
+    if(!e.target.dataset.category){
+        productContainer.innerHTML = '';
+        renderFilter();
+    } else {
+        renderFilter(e.target.dataset.category)
+    }
+}
+
+const init = () => {
+    categoryBtn.addEventListener('click', applyFilter)
+    // renderFilter("Hamburguesas")
+
+}
+
+init()
--- request.js
+++ request.js
@@ -2,7 +2,6 @@ const request = async () => {
     try {
         const response = await fetch("./assets/menu.json");
         const data = await response.json();
-        console.log(data)
         return data;  
 
     } catch {
@@ -10,5 +9,3 @@ const request = async () => {
     }
 
 }
-
-request()
