d4703f7e1324bab0ccd099cebe1bad355158591b
Jorge Ariel Castillo <j.a.castillo.jc@gmail.com>
2022-10-14 22:23:41 -0300

    se reparo id del menu.json y se agrego el request

--- request.js
+++ request.js
@@ -3,7 +3,6 @@ const request = async () => {
         const response = await fetch("./assets/menu.json");
         const data = await response.json();
         console.log(data)
-        console.log(data[5].Categoria, data[5].id, data[5].Nombre,data[5].Descripcion, data[5].Precio )
         return data;  
 
     } catch {
